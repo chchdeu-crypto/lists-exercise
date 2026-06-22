cefbe5df345401b13917e0ce50cd5190fd1743bf
chaim <chchdeu@gmail.com>
2026-06-22 14:30:21 +0300

    mission_9_agents

--- roster.py
+++ roster.py
@@ -1,20 +1,24 @@
-# number 1
-agents = ["Alpha", "bravo", "Charlie", "delta", "echo",]
-print(agents)
-#number 2
-print(agents[0],agents[4])
-#number 3
-print(agents[2])
-#number 4
-print(agents[1:4])
-#number 5
-agents.append("foxtrot")
-print(agents)
-#mission 6
-agents.insert(2,"zulu")
-print(agents)
-# mission 7
-agents.remove("bravo")
-print(agents)
-#mission 8
-print(len(agents))
+# # number 1
+# agents = ["Alpha", "bravo", "Charlie", "delta", "echo",]
+# print(agents)
+# #number 2
+# print(agents[0],agents[4])
+# #number 3
+# print(agents[2])
+# #number 4
+# print(agents[1:4])
+# #number 5
+# agents.append("foxtrot")
+# print(agents)
+# #mission 6
+# agents.insert(2,"zulu")
+# print(agents)
+# # mission 7
+# agents.remove("bravo")
+# print(agents)
+# #mission 8
+# print(len(agents))
+#mission 9
+scores = [42,17,95,8,61]
+print(max(scores))
+print(min(scores))
