5c9a91bc03411fe6b0d783d94370b6aa09097fe6
chaim <chchdeu@gmail.com>
2026-06-22 14:14:21 +0300

    mission_4_agents

--- roster.py
+++ roster.py
@@ -1,5 +1,9 @@
 # number 1
 agents = ["Alpha", "bravo", "Charlie", "delta", "echo",]
 print(agents)
+#number 2
 print(agents[0],agents[4])
+#number 3
 print(agents[2])
+#number 4
+print(agents[1:4])
