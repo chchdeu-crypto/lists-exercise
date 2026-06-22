dd2b9850f4e49823de37c0171e142790870dbddf
chaim <chchdeu@gmail.com>
2026-06-22 15:37:59 +0300

    mission_3

--- optional advaced basics.py
+++ optional advaced basics.py
@@ -1,13 +1,21 @@
-# #miision 1
-# numbers = [3,1,4,1,5,9,2,6,]
-# numbers.sort()
-# print(numbers)
-# numbers_1 = [3,1,4,1,5,9,2,6,]
-# print(sorted(numbers_1))
-# print(numbers_1)
-#mission 2
-a = [1,2,3,]
-b = [4,5,6,]
-print(a + b)
-a.extend(b)
-print(a)
+# # #miision 1
+# # numbers = [3,1,4,1,5,9,2,6,]
+# # numbers.sort()
+# # print(numbers)
+# # numbers_1 = [3,1,4,1,5,9,2,6,]
+# # print(sorted(numbers_1))
+# # print(numbers_1)
+# #mission 2
+# a = [1,2,3,]
+# b = [4,5,6,]
+# print(a + b)
+# a.extend(b)
+# print(a)
+#mission 3
+items = ["x","y","z","x","y","x"]
+x = items.count("x")
+print(x)
+items.remove("x")
+items.remove("x")
+items.remove("x")
+print(items)
