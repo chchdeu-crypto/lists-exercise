05b0df6db36722bbabe42452fc1055cc11fd2422
chaim <chchdeu@gmail.com>
2026-06-22 16:26:07 +0300

    final

--- roster.py
+++ roster.py
@@ -1,29 +1,29 @@
 # # number 1
-# agents = ["Alpha", "bravo", "Charlie", "delta", "echo",]
-# print(agents)
-# #number 2
-# print(agents[0],agents[4])
-# #number 3
-# print(agents[2])
-# #number 4
-# print(agents[1:4])
-# #number 5
-# agents.append("foxtrot")
-# print(agents)
-# #mission 6
-# agents.insert(2,"zulu")
-# print(agents)
-# # mission 7
-# agents.remove("bravo")
-# print(agents)
-# #mission 8
-# print(len(agents))
+agents = ["Alpha", "bravo", "Charlie", "delta", "echo",]
+print(agents)
+#number 2
+print(agents[0],agents[4])
+#number 3
+print(agents[2])
+#number 4
+print(agents[1:4])
+#number 5
+agents.append("foxtrot")
+print(agents)
+#mission 6
+agents.insert(2,"zulu")
+print(agents)
+# mission 7
+agents.remove("bravo")
+print(agents)
+#mission 8
+print(len(agents))
 #mission 9
 scores = [42,17,95,8,61]
 print(max(scores))
 print(min(scores))
 #mission 10
-new_scors = scores.copy()
-new_scors[0] = "11"
-print(scores)
-print(new_scors)
+new_agents = agents.copy()
+new_agents[0] = "chaim"
+print(agents)
+print(new_agents)
