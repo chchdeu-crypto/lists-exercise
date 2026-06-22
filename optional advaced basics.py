0e7015289ff01b9bfa69dd7b0fb868ab88ff4966
chaim <chchdeu@gmail.com>
2026-06-22 15:19:37 +0300

    mission_2

--- optional advaced basics.py
+++ optional advaced basics.py
@@ -1,7 +1,13 @@
-#miision 1
-numbers = [3,1,4,1,5,9,2,6,]
-numbers.sort()
-print(numbers)
-numbers_1 = [3,1,4,1,5,9,2,6,]
-print(sorted(numbers_1))
-print(numbers_1)
+# #miision 1
+# numbers = [3,1,4,1,5,9,2,6,]
+# numbers.sort()
+# print(numbers)
+# numbers_1 = [3,1,4,1,5,9,2,6,]
+# print(sorted(numbers_1))
+# print(numbers_1)
+#mission 2
+a = [1,2,3,]
+b = [4,5,6,]
+print(a + b)
+a.extend(b)
+print(a)
