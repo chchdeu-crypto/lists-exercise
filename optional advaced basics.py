bd8c4428621d54a2e502102b585a81d86caf5156
chaim <chchdeu@gmail.com>
2026-06-22 15:54:11 +0300

    final

--- optional advaced basics.py
+++ optional advaced basics.py
@@ -1,27 +1,27 @@
 # # # #miision 1
-# # # numbers = [3,1,4,1,5,9,2,6,]
-# # # numbers.sort()
-# # # print(numbers)
-# # # numbers_1 = [3,1,4,1,5,9,2,6,]
-# # # print(sorted(numbers_1))
-# # # print(numbers_1)
+numbers = [3,1,4,1,5,9,2,6,]
+numbers.sort()
+print(numbers)
+numbers_1 = [3,1,4,1,5,9,2,6,]
+print(sorted(numbers_1))
+print(numbers_1)
 # # #mission 2
-# # a = [1,2,3,]
-# # b = [4,5,6,]
-# # print(a + b)
-# # a.extend(b)
-# # print(a)
+a = [1,2,3,]
+b = [4,5,6,]
+print(a + b)
+a.extend(b)
+print(a)
 # #mission 3
-# items = ["x","y","z","x","y","x"]
-# x = items.count("x")
-# print(x)
-# items.remove("x")
-# items.remove("x")
-# items.remove("x")
-# print(items)
+items = ["x","y","z","x","y","x"]
+x = items.count("x")
+print(x)
+items.remove("x")
+items.remove("x")
+items.remove("x")
+print(items)
 # #mission 4
-# data = [1,2,3,4,5,]
-# print(data[0::2])
+data = [1,2,3,4,5,]
+print(data[0::2])
 #mission 5
 matrix = [[1,2,3,],[4,5,6,],[7,8,9,]]
 print(matrix[1][1])
